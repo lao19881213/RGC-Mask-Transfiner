db7c5ed0455434863f7860b67d8f77f4f5a19e71
lao19881213 <157762219@qq.com>
2022-11-30 15:23:27 +0800

    successful run on gznu using 3 GPUs

---
+++ scripts/train_transfiner_3x_101_deform_plain_gznu_3gpu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+#python3 setup.py build develop #--no-deps
+# python3 setup.py develop #--no-deps
+
+cd /home/data0/lbq/RGC-Mask-Transfiner
+
+source /home/data0/lbq/RGC-Mask-Transfiner/bashrc_gznu
+#python3 setup.py build develop #--no-deps
+# python3 setup.py develop #--no-deps
+
+#export PYTHONPATH=$PYTHONPATH:`pwd`
+#export CUDA_LAUNCH_BLOCKING=1 # for debug
+
+ID=159
+
+CUDA_VISIBLE_DEVICES=0,1,2 python3 tools/plain_train_net.py --num-gpus 3 --resume --dist-url tcp://0.0.0.0:12349\
+	--config-file configs/transfiner/mask_rcnn_R_101_FPN_3x_deform.yaml 
+
