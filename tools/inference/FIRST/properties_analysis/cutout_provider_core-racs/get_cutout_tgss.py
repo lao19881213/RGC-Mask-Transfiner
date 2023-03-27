1851658d09cd6d0ea288b048e4a8798a52b96316
lao19881213 <157762219@qq.com>
2023-03-27 12:12:01 +0800

    updates download

--- tools/inference/FIRST/properties_analysis/cutout_provider_core-racs/get_cutout_tgss.py
+++ tools/inference/FIRST/properties_analysis/cutout_provider_core-racs/get_cutout_tgss.py
@@ -4,36 +4,55 @@
 
 import pyvo as vo
 import subprocess
+import argparse
+import pandas as pd
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--resultcsv', help='hetu results file')
+parser.add_argument('--outdir', help='output directory')
+args = parser.parse_args()
+
+#######################################
+# Input text file should have RA DEC
+#######################################
+hetu_data = pd.read_csv(args.resultcsv)
+
+labels = hetu_data['label'].values
+ras = hetu_data['centre_ra'].values
+decs = hetu_data['centre_dec'].values
+boxs = hetu_data['box'].values
+names = hetu_data['source_name'].values
+
+clns = {'fr1': 'FRI', 'fr2': 'FRII', 'ht': 'HT', 'cj': 'CJ'}
 
 vo.__file__
 url='https://vo.astron.nl/tgssadr/q_fits/imgs/siap.xml'
 
-#######################################
-# Input text file should have ID RA DEC
-# Edit the name of the input file here:
-input_file = "id_ra_dec.txt"
-#######################################
 
-query = vo.sia.SIAQuery(url)
-query.format = 'image/fits'
-
-#######################################
-# Choose the cutout size (degrees)
-query.size = 0.1
-#######################################
-
-with open(input_file,'r') as te:
-    for line in te:
-        name=line.split()[0]
-        ra=line.split()[1]
-        dec=line.split()[2]
-
-        query.pos = (float(ra), float(dec))
-
-        results = query.execute()
-
-        for image in results:
-            print "Downloading %s..." %name
-            image.cachedataset(filename="%s.fits" %name)
+for m in range(len(labels)):
+    for cln in clns.keys():
+        #print(cls)
+        if(labels[m]==cln):
+           os.chdir('%s/%s/TGSS' % (args.outdir, clns[cln]))
+           query = vo.sia.SIAQuery(url)
+           query.format = 'image/fits'
+           
+           #######################################
+           # Choose the cutout size (degrees)
+           query.size = 0.1
+           #######################################
+           
+           name=names[m]
+           ra=ras[m]
+           dec=decs[m]
+           #print(float(ra), float(dec)) 
+           query.pos = (float(ra), float(dec))
+           
+           results = query.execute()
+           #print(results)
+           for image in results:
+               print ("Downloading %s..." %name)
+               image.cachedataset(filename="TGSS_%s.fits" %name)
 
 print("All done!")
--- tools/inference/FIRST/properties_analysis/cutout_provider_core-racs/get_cutout_tgss.sh
+++ tools/inference/FIRST/properties_analysis/cutout_provider_core-racs/get_cutout_tgss.sh
@@ -2,7 +2,6 @@
 
 
 export PATH=/home/data0/lbq/software/anaconda3/bin:$PATH
-export PATH=/home/data0/lbq/software/Montage-6.0/bin:$PATH
 
-python cutout_hetu.py --resultcsv /home/data0/lbq/RGC-Mask-Transfiner/FIRST_results/FIRST_HeTu_paper.csv \
+python get_cutout_tgss.py --resultcsv /home/data0/lbq/RGC-Mask-Transfiner/FIRST_results/FIRST_HeTu_paper.csv \
                       --outdir /home/data0/lbq/RGC-Mask-Transfiner/tools/inference/FIRST/properties_analysis 
