7ecec2cbf8e70937ee88959968887a3b8e5faa2e
lao19881213 <157762219@qq.com>
2023-08-01 23:32:12 +0800

    update radio optical overlay

--- tools/inference/FIRST/properties_analysis/cutout_provider_core-racs/cutout_hetu_vlass_added.sh
+++ tools/inference/FIRST/properties_analysis/cutout_provider_core-racs/cutout_hetu_vlass_added.sh
@@ -5,6 +5,6 @@ export PATH=/home/data0/lbq/software/anaconda3-cutout/bin:$PATH
 export PATH=/home/data0/lbq/software/Montage-6.0/bin:$PATH
 
 python cutout_hetu_vlass_added.py --resultcsv /home/data0/lbq/RGC-Mask-Transfiner/FIRST_results/FIRST_HeTu_paper_cj.csv \
-                      --outdir /home/data0/lbq/RGC-Mask-Transfiner/tools/inference/FIRST/properties_analysis/CJ_add \
+                      --outdir /home/data0/lbq/RGC-Mask-Transfiner/tools/inference/FIRST/properties_analysis/CJ/CJ_add \
                       --surveys '1'
 
