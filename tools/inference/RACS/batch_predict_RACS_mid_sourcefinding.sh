cfd5904d319e8ea154e9e2c02b8a52ef4c689733
lao19881213 <157762219@qq.com>
2023-11-08 15:19:14 +0800

    fixed batch predict RACS-mid

--- tools/inference/RACS/batch_predict_RACS_mid_sourcefinding.sh
+++ tools/inference/RACS/batch_predict_RACS_mid_sourcefinding.sh
@@ -15,7 +15,7 @@ DATA_DIR="/home/data0/lbq/inference_data/ASKAP_RACS/RACS_mid"
 for fn in `cat $IMG_FILES`;
 do
 SBID=${fn: 21:7}
-if [ ! -f "/home/data0/lbq/RGC-Mask-Transfiner/RACS_results/RACS-mid_${fn}.csv" ];then
+if [ ! -f "/home/data0/lbq/RGC-Mask-Transfiner/RACS_results/RACS-mid_${SBID}.csv" ];then
 
    CUDA_VISIBLE_DEVICES=0 python3 demo/batch_predict.py --config-file configs/transfiner/mask_rcnn_R_101_FPN_3x_deform.yaml \
      --input ${DATA_DIR}/${SBID}_split_fits_png \
