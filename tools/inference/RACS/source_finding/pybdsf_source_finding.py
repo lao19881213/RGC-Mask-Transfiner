f65e0673cfe51f52330196930119e493faed4745
lao19881213 <157762219@qq.com>
2023-11-23 14:53:52 +0800

    added pybdsf source finding for racs

---
+++ tools/inference/RACS/source_finding/pybdsf_source_finding.py
@@ -0,0 +1,34 @@
+import bdsf
+import sys
+import os
+
+img_list = '/home/data0/lbq/inference_data/ASKAP_RACS/RACS_mid/part1_fits_fixed.txt'
+
+data_dir = '/home/data0/lbq/inference_data/ASKAP_RACS/RACS_mid/part1_conv'
+
+cat_dir = '/home/data0/lbq/inference_data/ASKAP_RACS/RACS_mid/part1_pybdsf'
+
+with open(img_list) as f:
+     for line in f:
+         fits_n = line.split('\n')[0]
+         cat_n = fits_n.split('.')[3]
+         if os.path.isfile('%s/%s_gaussian_list.csv' % (cat_dir, cat_n)) and os.path.isfile('%s/%s_source_list.csv' % (cat_dir, cat_n)):
+            print('catalogs already exists for %s' % cat_n)
+         else: 
+            img = bdsf.process_image('%s/%s' % (data_dir, fits_n), 
+                         thresh = 'hard', 
+                         advanced_opts=True, 
+                         rms_box = (150, 30), 
+                         atrous_do = True,
+                         atrous_jmax = 3,
+                         mean_map = 'zero',
+                         group_by_isl = True)
+            
+            #cat_n = fits_n.split('.')[3]
+            img.write_catalog(outfile='%s/%s_gaussian_list.csv' % (cat_dir, cat_n), 
+                    catalog_type='gaul', clobber=True, format='csv')
+            img.write_catalog(outfile='%s/%s_source_list.csv' % (cat_dir, cat_n), 
+                    catalog_type='srl', clobber=True, format='csv')
+
+
+#img.show_fit()
